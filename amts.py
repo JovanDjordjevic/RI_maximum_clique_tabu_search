6ad010515622423113b71358edce1e1f805147e1
Jovan Djordjevic <joca.dj@hotmail.com>
2022-02-03 14:53:53 +0100

    tmp

--- amts.py
+++ amts.py
@@ -16,10 +16,7 @@ def constructInitialSolution(S:set, notInS:set, g:Graph, frequencies, k):
 
     V = g.getNodeCount() + 1
 
-   # start = timeit.default_timer()
-
-    # NOTE: ono sto je u komentaru ispod je bilo ovde pre reworka
-    # mapa gde je kljuc stepen cvora cvora ka S, a vrednost skup cvorova sa tim stepenom
+    # mapa gde je kljuc stepen cvora ka S, a vrednost skup cvorova sa tim stepenom
     degreeSets = defaultdict(set)
     degreesTowardsS = [0 for _ in range(V)]
     maxDegree = 0
@@ -33,15 +30,6 @@ def constructInitialSolution(S:set, notInS:set, g:Graph, frequencies, k):
             maxDegree = degreesTowardsS[i]
 
     while len(S) < k:
-        #print(len(S), maxDegree, len(degreeSets[maxDegree]))
-        #print(frequencies)
-        #print(max(degreesTowardsS))
-        #print(notInS)
-        #if (maxDegree > max(degreesTowardsS)):
-        #   print(maxDegree)
-        #   print(max(degreesTowardsS))
-        #   print()
-
         candidates = []
         minFreq = float('inf')  
         for i in degreeSets[maxDegree]:          
@@ -52,18 +40,12 @@ def constructInitialSolution(S:set, notInS:set, g:Graph, frequencies, k):
             elif frequencies[i] == minFreq:
                 candidates.append(i)
 
-        nodeToAdd = random.choice(candidates)       # FIXME: c-fat200-5 puca u nekim slicajevima u ovoj liniji, kaze da je candidates prazan niz Ako se koristi ovaj neoptimizovani
-                                                    # pristup ispod, radi kako treba
-                                                    # probao sam jedno 200 puta da pokrenem za ovaj graf nakon sto sam u mainu smanjo L, i posle toga se nije desilo nijednom
+        nodeToAdd = random.choice(candidates)
+
         S.add(nodeToAdd)
         notInS.remove(nodeToAdd)
 
-        #print("pre:",nodeToAdd, maxDegree, max(degreesTowardsS))
-        #print(degreesTowardsS)
         degreesTowardsS[nodeToAdd] = 0
-        #print("posle", maxDegree, max(degreesTowardsS))
-        #print(degreesTowardsS)
-        #print()
 
         degreeSets[maxDegree].remove(nodeToAdd)
         while len(degreeSets[maxDegree]) == 0:
@@ -76,34 +58,6 @@ def constructInitialSolution(S:set, notInS:set, g:Graph, frequencies, k):
                 degreeSets[degreesTowardsS[i]].add(i)
                 if degreesTowardsS[i] > maxDegree:
                     maxDegree = degreesTowardsS[i]
-        
-
-
-    # while len(S) < k:
-    #     degreesTowardsS = [0 for _ in range(V)]
-    #     maxDegree = 0
-    #     for i in notInS:
-    #         for j in S:
-    #             if adjMatrix[i][j] == 1:
-    #                 degreesTowardsS[i] += 1
-    #                 if degreesTowardsS[i] > maxDegree:
-    #                     maxDegree = degreesTowardsS[i]
-        
-    #     # oni sa najvecim brojem grana koji vode ka S a nisu vec u S
-    #     candidates = [i for i in range(1, V) if degreesTowardsS[i] == maxDegree]
-    #     candidates = sorted(candidates, key = lambda i : frequencies[i])
-
-    #     minFrequency = frequencies[candidates[0]]
-    #     candidates = [i for i in candidates if frequencies[i] == minFrequency]
-
-    #     nodeToAdd = random.choice(candidates)
-    #     S.add(nodeToAdd)
-    #     notInS.remove(nodeToAdd)
-
-
-
-    #end = timeit.default_timer()
-    #print(f"initial sol: {end-start}")
     return    
 
 
@@ -115,8 +69,6 @@ def generateConstrainedNeighborhood(tabuList:list, g:Graph, S:set, notInS:set, d
     V = g.getNodeCount() + 1
     adjMatrix = g.getAdjacencyMatrix()
 
-    # da je neki cvor tabu tojest nedostupan gledamo takos to pitamod da li je tabuList[cvor] > currentIteration
-    # zato sto je tabuList[cvor] = tenure_cvora + iteracija u kojoj je dodat u tabu listu
     for i in S:
         if tabuList[i] < currentIteration and degreesTowardsS[i] < minInS:   
             minInS = degreesTowardsS[i]
@@ -124,13 +76,9 @@ def generateConstrainedNeighborhood(tabuList:list, g:Graph, S:set, notInS:set, d
     for i in notInS:
         if tabuList[i] < currentIteration and degreesTowardsS[i] > maxOutS:   
             maxOutS = degreesTowardsS[i]
-    # print("minInS: ", minInS)
-    # print("maxOutS: ", maxOutS)
 
     A = [u for u in S if tabuList[u] < currentIteration and degreesTowardsS[u] == minInS]
     B = [v for v in notInS if tabuList[v] < currentIteration and degreesTowardsS[v] == maxOutS]
-    # print("A: ", A)
-    # print("B: ", B)
 
     # constrained neigborhood je lista svih mogucih zamenena cvora iz A sa cvorom iz B
     # delta je promena vrednosti funkcije f(S) ako se taj potez primeni, f(newS) = f(S) + delta
@@ -140,11 +88,8 @@ def generateConstrainedNeighborhood(tabuList:list, g:Graph, S:set, notInS:set, d
             delta = maxOutS - minInS
             if adjMatrix[i][j] == 1:
                 delta -= 1
-            # ako zamenimo i sa j, dobijamo promenu delta
             constrainedNeighborhood.append([i, j, delta])
-    
     #print(constrainedNeighborhood)
-
     return constrainedNeighborhood, A, B, minInS, maxOutS
 
 
@@ -153,11 +98,7 @@ def calcTenures(fS:int, k:int):
     l = min(10, k * (k - 1) / 2 - fS)
     C = max(int( np.floor(k/40) ), 6)
     tabuTenureU = int( l + random.randint(1, C - 1) )
-    tabuTenureV = int( np.floor( 0.6 * l + random.randint(1, np.floor(0.6 * (C - 1)) ) ) ) # ima mi smisla da bude floor jer samo ceo broj moze da bude broj iteracija
-    # treba uvek da vazi da Tu > Tv
-    # print("Tu: ", tabuTenureU)
-    # print("Tv: ", tabuTenureV)
-
+    tabuTenureV = int( np.floor( 0.6 * l + random.randint(1, np.floor(0.6 * (C - 1)) ) ) )
     return tabuTenureU, tabuTenureV
 
 
@@ -192,8 +133,7 @@ def ts_0 (g, S, notInS, frequencies, k , L, iteration, maxIters):
                 degreesTowardsS[i] += 1
 
     while (tabuIters < L and tabuIters < maxIters):
-        
-        iterStart = timeit.default_timer()
+        #iterStart = timeit.default_timer()
 
         # posto l i C zavise od funkcije fS, ima smisla ne racunati ih stalno vec 
         # fS = fBestS samo kada se nadje novi najbolji
@@ -206,7 +146,6 @@ def ts_0 (g, S, notInS, frequencies, k , L, iteration, maxIters):
         desireableDelta = maxOutS - minInS
 
         T = [move for move in CN if move[2] == desireableDelta]
-        #print("T: ", T)
 
         bestMove = [-1, -1, -1]   # temp vrednost
 
@@ -232,8 +171,7 @@ def ts_0 (g, S, notInS, frequencies, k , L, iteration, maxIters):
             delta = maxOutS - minInS
             if adjMatrix[u][v] == 1:
                 delta -= 1
-            bestMove = [u, v, delta]  #  Notice that  in this latter case, u and v must be two adjacent vertices.   (jel ovo treba da proverim?)
-            #print("Best move: ", bestMove)
+            bestMove = [u, v, delta]
 
         u = bestMove[0]
         v = bestMove[1]
@@ -272,7 +210,7 @@ def ts_0 (g, S, notInS, frequencies, k , L, iteration, maxIters):
         else:
             tabuIters += 1
 
-        iterEnd = timeit.default_timer()
+        #iterEnd = timeit.default_timer()
         #print(fBestS, tabuIters, iteration, iterEnd-iterStart)
 
     return bestS, iteration
@@ -286,22 +224,17 @@ def amts (g:Graph, k:int, L:int, maxIters:int):
     notInS = deepcopy(g.getNodeSet())
 
     constructInitialSolution(S, notInS, g, frequencies, k)
-    # print(S)
-    # print(notInS)
 
-    # iteracije morajud a se broje od jedan u suprotnom ce A i B biti praznis kupovi u nultoj iteraciji
     iters = 1
     
     while (iters <= maxIters):
         newS, iters = ts_0(g, S, notInS, frequencies, k, L, iters, maxIters)
-        #print(f"Next found S: {newS}   fitness: {f(g, newS)}")
         if isValid(f(g, newS), k):
             #print("Returning newS: ", newS)
             return newS, iters
         else: 
             #print(f"Restarting on iteration {iters}")
-
-            # ovde proveri da li su sve freq vece od k i resetuj ih na 0 ako jesu
+            # ako su sve frekvencije vece od k, niz treba da se resetuje
             shouldResetFrequencies = True
             for i in range(1, g.getNodeCount() + 1):
                 if frequencies[i] < k:
@@ -315,8 +248,8 @@ def amts (g:Graph, k:int, L:int, maxIters:int):
             notInS = deepcopy(g.getNodeSet())
             constructInitialSolution(S, notInS, g, frequencies, k)
     
-    # ako se desi da prodje maxIters, vracamo najveci koji smo do sada videli 
-    return S, iters
+    # ako se desi da prodje maxIters, vracamo prazan set kako bi signalizirali neuspeh
+    return set(), iters
 
 
 
@@ -332,7 +265,5 @@ def f(g:Graph, S):
         for j in S:
             if adjMatrix[i][j] == 1:
                 f += 1
-    # u definiciji f u radu, kaze za svako u,v iz S se dodaje +1 ako postoji grana
-    # dakle racunaju se duplikati, ali ako tako ubacim u kod, tabu tenure budu negativni sto nije logicno
     f /= 2
     return f
--- main.py
+++ main.py
@@ -4,8 +4,130 @@ from matplotlib import pyplot as plt
 from amts import *
 from bruteforce import *
 import timeit
+import os
+
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing.pool import AsyncResult
+from multiprocessing.pool import ThreadPool
 
 if __name__ == "__main__":
+    # TODO: ubaciti da se bira opcija da li se pokrece bruteforce ili amts,
+    #  za neki ili za sve, da nadje k ili za zadato k
+
+
+    # kljuc: ime grafa, vrednost: max clique koji je pronadjen u literaturi
+    # TODO: naci bolji nacin za ovo
+    maxCliquesInLiterature = {
+        'brock200-1.mtx' : 21,
+        'brock200-2.mtx' : 12,
+        'brock200-3.mtx' : 15,
+
+        'johnson8-2-4.mtx' : 4,
+        
+        'hamming10-2.mtx' : 512,
+        'C500-9.mtx' : 57,
+        'c-fat200-5.mtx' : 58,
+        'hamming6-2.mtx' : 32,
+        'hamming6-4.mtx' : 4,
+    }
+    
+    alreadyTested = {
+        'brock200-1.mtx', 
+        'brock200-3.mtx',
+        
+        'johnson8-2-4.mtx',
+        'c-fat200-5.mtx',
+        'hamming6-2.mtx',
+        'hamming6-4.mtx',
+    }
+
+    # oni gde se desavalo da jedna traje vise od pola minuta
+    ignoreForNow = {
+        #'C500-9.mtx',
+        'hamming10-2.mtx',
+    }
+
+    for root, dirs, files in os.walk('data'):
+        for file in files:
+            if str(file).endswith('.mtx') and str(file) not in alreadyTested and str(file) not in ignoreForNow:
+                fileName = os.path.join(root, file)
+                # print(fileName)
+                g = Graph(fileName)
+                # print(f'Graph name: {fileName} Node count: {g.getNodeCount()}   Edgde count: {g.getEdgeCount()}    Graph density: {g.getDensity()}')
+
+                times = []
+                iterCounts = []
+                cliqueSizes = []
+                
+                k = maxCliquesInLiterature[str(file)]
+
+                maxIters = 100000                     # u radu korsiceno 10 ** 8 sto je za nas presporo
+                L = g.getNodeCount()                  # u radu korisceno   L = g.getNodeCount() * k   osim za neke 
+    
+                # recimo da hocemo informacije za 100 iteracija
+                for i in range(100):
+                    print(f"{file}, current iteration: {i}")
+                    
+                    timerStart = timeit.default_timer()
+
+                    resultClique, totalIterations = amts(g, k=k, L=L, maxIters=maxIters)
+                    # threadPool = ThreadPool(processes=8)
+                    # result1 = threadPool.apply_async(amts, [g, k, L, maxIters/2])
+                    # ans1 = result1.get()
+                    # result2 = threadPool.apply_async(amts, [g, k, L, maxIters/2])
+                    # ans2 = result2.get()
+                    # result3 = threadPool.apply_async(amts, [g, k, L, maxIters/2])
+                    # ans3 = result3.get()
+                    # print(ans1, ans2, ans3)
+                    # exit()
+
+                    timerEnd = timeit.default_timer()
+                    totalTime = timerEnd - timerStart
+
+                    print(f"Maximum found clique is of size {len(resultClique)}, found in {totalIterations} iterations:\n\n{resultClique}\n\n Elapsed time: {totalTime}")
+
+                    times.append(totalTime)
+                    cliqueSizes.append(len(resultClique))
+                    iterCounts.append(totalIterations)
+
+                avgTime = np.average(times)
+                avgIterations = np.average(iterCounts)
+                avgCliqueSize = np.average(cliqueSizes)
+
+                fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+                fig.suptitle("Results for graph " + fileName)
+                fig.set_size_inches(12, 6)
+                
+                _, bins, _ = ax1.hist(times)
+                ax1.set_title("Average time: " + str(avgTime))
+                ax1.set(xlabel="t", xticks=bins)
+                ax1.grid(True)
+
+                _, bins, _ = ax2.hist(cliqueSizes)
+                ax2.set_title( "Average size of found cliques: " + str(avgCliqueSize))
+                ax2.set(xlabel="clique size", xticks=bins)
+                ax2.grid(True)
+
+                _, bins, _ = ax3.hist(iterCounts)
+                ax3.set_title("Average needed iterations: " + str(avgIterations))
+                ax3.set(xlabel="iterations", xticks=bins)
+                ax3.grid(True)
+
+                plt.setp(ax1.get_xticklabels(), rotation=30, horizontalalignment='right')
+                plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right')
+                plt.setp(ax3.get_xticklabels(), rotation=30, horizontalalignment='right')
+                plt.tight_layout()
+                plt.savefig(fileName + '.png')
+                #plt.show()
+
+
+
+
+
+
+
+    exit()
 
     fileName = "data/johnson8-2-4.mtx"
     #fileName = "data/brock200-1.mtx"
@@ -55,6 +177,7 @@ if __name__ == "__main__":
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     fig.suptitle("Results for graph " + fileName)
+    fig.set_size_inches(12, 6)
     
     _, bins, _ = ax1.hist(times)
     ax1.set_title("Average time: " + str(avgTime))
@@ -75,8 +198,8 @@ if __name__ == "__main__":
     plt.setp(ax2.get_xticklabels(), rotation=30, horizontalalignment='right')
     plt.setp(ax3.get_xticklabels(), rotation=30, horizontalalignment='right')
     plt.tight_layout()
+    plt.savefig(fileName + '.png')
     plt.show()
-    
 
     ## da nadje k
     # maxK = -1
@@ -99,10 +222,10 @@ if __name__ == "__main__":
     #        maxClique = kClique
     #        maxCliqueTime = timerEnd - timerStart
 
-    # maxCliqueTime = -1
-    # timerStart = timeit.default_timer()
-    # maxC = maxClique_bruteforce(g, 0, 1)
-    # timerEnd = timeit.default_timer()
-    # maxCliqueTime = timerEnd - timerStart
-    # print()
-    # print(f"Maximum found clique is of size {maxC} with bruteforce algorithm, found in {maxCliqueTime} seconds.")
+    # # maxCliqueTime = -1
+    # # timerStart = timeit.default_timer()
+    # # maxC = maxClique_bruteforce(g, 0, 1)
+    # # timerEnd = timeit.default_timer()
+    # # maxCliqueTime = timerEnd - timerStart
+    # # print()
+    # # print(f"Maximum found clique is of size {maxC} with bruteforce algorithm, found in {maxCliqueTime} seconds.")
